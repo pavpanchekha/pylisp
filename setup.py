6655462b1d1833ae18403945a0d58137aee543a0
Pavel Panchekha <pavpanchekha@gmail.com>
2010-03-05 21:16:53 -0500

    setuptoolsified installer

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+import os
 
+stdlib = map(lambda x: "pylisp/stdlib/" + x, os.listdir("pylisp/stdlib"))
+tests = map(lambda x: "pylisp/test/" + x, os.listdir("pylisp/test"))
+data_files = [
+        ("pylisp/stdlib", stdlib),
+        ("pylisp/test", tests)]
+
+setup(
+        name = "Pylisp",
+        version = "0.1",
+        author = "pavpanchekha",
+        author_email = "pavpanchekha@gmail.com",
+        packages = ["pylisp"],
+        scripts = ["pylisp/pylisp"],
+        url = "http://pypi.python.org/pypi/pylisp/",
+        license = "LICENSE.txt",
+        description = "A lisp for pythonistas",
+        long_description = open("README.txt").read(),
+        data_files = data_files,
+    )
