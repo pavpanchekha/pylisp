0f298d0208ad873cce3ddcc73eb4ca5fcc4d4765
Pavel Panchekha <pavpanchekha@gmail.com>
2010-03-27 00:32:33 -0400

    Revert "Added links to inheritdict for performance" --- segfaults
    This reverts commit 9ab9a259c9e6de7a78ca75eb92ec999ee18e9c85.

--- pylisp/inheritdict.py
+++ pylisp/inheritdict.py
@@ -1,65 +1,33 @@
-class _link(object):
-    __slots__ = ["var", "dict"]
-
-    def __init__(self, var, dict):
-        self.var = var
-        self.dict = dict
-
-    def value(self):
-        return self.dict[self.var]
-
-    @classmethod
-    def new(cls, var, dict):
-        if isinstance(dict[var], cls):
-            return dict[var]
-        else:
-            return cls(var, dict)
-
-class idict(object):
+class idict:
     def __init__(self, parent=None, odict=None, **kwargs):
         if parent is None:
             parent = {}
-        if odict: kwargs = odict
+        if kwargs: odict = kwargs
 
         self.parent = parent
-        self.dict = kwargs
+        self.dict = odict
         self.stop = False
 
     def __getitem__(self, item):
         if item in self.dict:
-            k = self.dict[item]
-            if isinstance(k, _link):
-                k = k.value()
+            return self.dict[item]
         else:
-            k = self.parent[item]
-            self.dict[item] = _link.new(item, self.parent)
-        return k
+            return self.parent[item]
 
     def __setitem__(self, item, value):
-        if self.stop:
-            self.dict[item] = value
-        elif item in self.dict and isinstance(self.dict[item], _link):
-            self.dict[item].dict.dict[item] = value
-        elif item not in self.parent:
+        if self.stop or item not in self.parent:
             self.dict[item] = value
         else:
             self.parent[item] = value
-            self.dict[item] = _link.new(item, self.parent)
 
     def __delitem__(self, item):
         if item in self.dict:
-            if isinstance(self.dict[item], _link):
-                del self.dict[item].dict[item]
             del self.dict[item]
         else:
             del self.parent[item]
 
     def __contains__(self, item):
-        k = item in self.dict
-        if k: return _link.new(item, self.dict)
-        k = item in self.parent
-        if k: return k
-        return False
+        return item in self.dict or item in self.parent
 
     def push(self, kwargs=None):
         if kwargs is None: kwargs = {}
