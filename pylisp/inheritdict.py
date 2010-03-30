dcca9d92f5af2e1782847bfafed83498528f711b
Pavel Panchekha <pavpanchekha@gmail.com>
2010-03-30 09:34:43 -0400

    Cache links in idicts

--- pylisp/inheritdict.py
+++ pylisp/inheritdict.py
@@ -1,4 +1,30 @@
-class idict:
+class link(object):
+    __slots__ = ["dict", "var"]
+
+    def __init__(self, dict, var):
+        self.var = var
+        self.dict = dict
+
+    def get(self):
+        return self.dict[self.var]
+
+    def set(self, value):
+        self.dict[self.var] = value
+
+    def delete(self, value):
+        del self.dict[self.var]
+
+    def exists(self):
+        return self.var in self.dict
+
+    @classmethod
+    def new(cls, dict, var):
+        if isinstance(dict[var], cls):
+            return dict[var]
+        else:
+            return cls(dict, var)
+
+class idict(object):
     def __init__(self, parent=None, odict=None, **kwargs):
         if parent is None:
             parent = {}
@@ -7,27 +33,52 @@ class idict:
         self.parent = parent
         self.dict = odict
         self.stop = False
+        self.cache = {}
 
     def __getitem__(self, item):
         if item in self.dict:
             return self.dict[item]
+        elif item in self.cache:
+            try:
+                l = self.cache[item]
+                return l.dict[l.var]
+            except KeyError:
+                del self.cache[item]
+                return self[item]
         else:
-            return self.parent[item]
+            v = self.parent[item]
+            self.cache[item] = link.new(self.parent, item)
+            return v
 
     def __setitem__(self, item, value):
-        if self.stop or item not in self.parent:
+        if item in self.cache and not self.cache[item].dict.stop:
+            try:
+                return self.cache[item].set(value)
+            except KeyError:
+                del self.cache[item]
+                self.dict[item] = value
+        if self.parent.stop or item not in self.parent:
             self.dict[item] = value
         else:
             self.parent[item] = value
+            self.cache[item] = link.new(self.parent, item)
 
     def __delitem__(self, item):
-        if item in self.dict:
+        if item in self.cache:
+            try:
+                self.cache[item].delete()
+            except KeyError:
+                pass
+            del self.cache[item]
+        elif item in self.dict:
             del self.dict[item]
         else:
             del self.parent[item]
 
     def __contains__(self, item):
-        return item in self.dict or item in self.parent
+        try: self[item]
+        except KeyError: return False
+        else: return True
 
     def push(self, kwargs=None):
         if kwargs is None: kwargs = {}
